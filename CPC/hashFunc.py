090328d47adae6c0d0dc8fa163e6dc04aa2f92eb
soyZambo <adrian.soto1@upr.edu>
2022-04-22 01:12:01 -0400

    Hash Function updated

--- CPC/hashFunc.py
+++ CPC/hashFunc.py
@@ -5,9 +5,17 @@ def hashFunc():
     toHash= str(now.day) + str(now.hour) + str(now.minute)
     
     if now.minute % 5==0:
-        return str(hash(toHash))
+        hashed = str(hash(toHash))
+        if hashed<0:
+            hashed = hashed * -1
+        return hashed
+    
         
     else:
         print("Not a multiple of 5, rounding... \n")
         rounded = 5 * round( hash(toHash) / 5)
+        if rounded<0:
+            rounded = rounded * -1
         return str(rounded)
+if __name__=="__main__":
+    print(hashFunc())
